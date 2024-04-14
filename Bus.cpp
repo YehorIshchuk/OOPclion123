deb000fd10014efbb9ac1bd6012bc83b874ad8bc
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-04-14 14:34:07 +0300

    Add a class Bus cpp and header file

---
+++ Bus.cpp
@@ -0,0 +1,7 @@
+#include "Bus.h"
+#include <iostream>
+using namespace std;
+
+void Bus::drive() {
+    cout << "Bus is delivering food" << std::endl;
+}
---
+++ Bus.h
@@ -0,0 +1,15 @@
+
+
+#ifndef LAB5_BUS_H
+#define LAB5_BUS_H
+#include "Vehicle.h"
+
+
+class Bus : public Vehicle {
+public:
+
+    void drive() override;
+
+};
+
+#endif //LAB5_BUS_H
