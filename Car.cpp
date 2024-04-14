b276d27e727de204ca2370c54f56bde6b8421ff7
YehorIshchuk <159074080+YehorIshchuk@users.noreply.github.com>
2024-04-14 14:33:35 +0300

    Add a class Car cpp and header files

---
+++ Car.cpp
@@ -0,0 +1,8 @@
+#include "Car.h"
+#include <iostream>
+
+using namespace std;
+
+void Car::drive() {
+    cout << "Car is delivering food" << std::endl;
+}
---
+++ Car.h
@@ -0,0 +1,13 @@
+
+#ifndef LAB5_CAR_H
+#define LAB5_CAR_H
+#include "Vehicle.h"
+using namespace std;
+
+class Car : public Vehicle {
+public:
+
+    void drive() override;
+};
+
+#endif //LAB5_CAR_H
